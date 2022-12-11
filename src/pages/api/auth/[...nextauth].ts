db22d1c302825547903e4676b81a531e6f228e14
Blayne Marjama <blayne.marjama@gmail.com>
2022-12-11 02:10:37 -0800

    supabase auth is up

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,8 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions, Session } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import db from "@/core/db";
+import supabase from "@/core/clients/supabase";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
@@ -9,6 +10,13 @@ export const authOptions: NextAuthOptions = {
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
+      sendVerificationRequest: async ({ identifier: email, url }) => {
+        // Send magic link to user from Supabase.
+        await supabase.auth.signInWithOtp({
+          email,
+          options: { emailRedirectTo: url },
+        });
+      },
     }),
   ],
   callbacks: {
