783cf22736e299ac4230e711ae90d1696a4debc1
alex bergvall <bergvall95@gmail.com>
2022-12-28 16:02:56 -0600

    deleted mobile app from this branch

--- ai-pet-photo-app/navigationConfig.ts
+++
@@ -1,9 +0,0 @@
-import { createNavigationContainerRef } from "@react-navigation/native";
-import { RootStackParamList } from "./types";
-
-export const navigationRef = createNavigationContainerRef<RootStackParamList>();
-export function navigate(name: any, params?: object) {
-  if (navigationRef.isReady()) {
-    navigationRef.navigate(name, params);
-  }
-}
--- ai-pet-photo-app/state/actions/Theme/toggleDarkModeAction.ts
+++
@@ -1,8 +0,0 @@
-import { Theme } from "../../../styles";
-
-export const TOGGLE_DARK_MODE = "TOGGLE_DARK_MODE";
-export const toggleDarkModeAction = (darkMode: boolean) =>
-  ({
-    type: TOGGLE_DARK_MODE,
-    darkMode,
-  } as const);
