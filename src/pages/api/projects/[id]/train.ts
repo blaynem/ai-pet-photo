e2343889d80aea0166f345df7fc52a00c694e1bb
Blayne Marjama <blayne.marjama@gmail.com>
2022-12-28 22:19:59 -0800

    updated training params

--- src/pages/api/projects/[id]/train.ts
+++ src/pages/api/projects/[id]/train.ts
@@ -1,11 +1,11 @@
 import db from "@/core/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
-import replicateClient, { TrainingResponse } from "@/core/clients/replicate";
-import {
-  getRefinedInstanceClass,
-  getTrainCoefficient,
-} from "@/core/utils/predictions";
+import replicateClient, {
+  TrainingRequest,
+  TrainingResponse,
+} from "@/core/clients/replicate";
+import { getRefinedInstanceClass } from "@/core/utils/predictions";
 import supabase from "@/core/clients/supabase";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -25,37 +25,41 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     },
   });
 
-  const MAX_IMAGES = 45; // Doesn't actually match the max number of images, but we want to set a limit on the steps
   const instanceClass = getRefinedInstanceClass(project.instanceClass);
-  const numOfImages = getTrainCoefficient(project.imageUrls.length);
-  const MAX_NUM_STEPS = Math.min(4500, Math.min(numOfImages, MAX_IMAGES) * 80);
+  // const numOfImages = getTrainCoefficient(project.imageUrls.length);
+  // const MAX_NUM_STEPS = Math.min(2000, Math.min(numOfImages, MAX_IMAGES) * 80);
+  const MAX_NUM_STEPS = 2020; // Let's not get fancy, let's just set it to the standard.
 
   const { data: data_url } = supabase.storage
     .from(process.env.NEXT_PUBLIC_UPLOAD_BUCKET_NAME!)
     .getPublicUrl(`${session.user.id}/${project.id}.zip`);
 
+  const trainingData: TrainingRequest = {
+    input: {
+      instance_prompt: `${project.instanceName} ${instanceClass}`,
+      class_prompt: `${instanceClass}`,
+      instance_data: data_url.publicUrl,
+      // num_class_images: numOfImages * 12,
+      num_class_images: 200, // We're just going to use 200 for now
+      max_train_steps: MAX_NUM_STEPS,
+      lr_warmup_steps: Math.round(MAX_NUM_STEPS / 10),
+      learning_rate: 1e-6,
+      lr_scheduler: "constant",
+      gradient_accumulation_steps: 1,
+      gradient_checkpointing: true,
+      train_batch_size: 1,
+      train_text_encoder: true,
+      // class_data: undefined, // Training data of the class image ex: 'dog' or 'person'
+      // resolution: 512, // I don't know what resolution we're using rn
+    },
+    model: `${process.env.REPLICATE_USERNAME}/${project.name}`,
+    webhook_completed: `${process.env.NEXTAUTH_URL}/api/webhooks/completed`,
+  };
+
+  // $13.43 -> 20.02 = 6.59
   const responseReplicate = await replicateClient.post<TrainingResponse>(
     "/v1/trainings",
-    {
-      input: {
-        instance_prompt: `${project.instanceName} ${instanceClass}`,
-        class_prompt: `${instanceClass}`,
-        instance_data: data_url.publicUrl,
-        num_class_images: numOfImages * 12,
-        max_train_steps: MAX_NUM_STEPS,
-        lr_warmup_steps: Math.round(MAX_NUM_STEPS / 10),
-        learning_rate: 1e-6,
-        lr_scheduler: "polynomial",
-        gradient_accumulation_steps: 1,
-        gradient_checkpointing: true,
-        train_batch_size: 1,
-        train_text_encoder: true,
-        // class_data: undefined, // Training data of the class image ex: 'dog' or 'person'
-        // resolution: 512, // I don't know what resolution we're using rn
-      },
-      model: `${process.env.REPLICATE_USERNAME}/${project.name}`,
-      webhook_completed: `${process.env.NEXTAUTH_URL}/api/webhooks/completed`,
-    },
+    trainingData,
     {
       headers: {
         Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
